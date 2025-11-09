fb5ce11de134be347b20fa262b45fdb90b816e9b
mannanswe <147591955+mannanswe@users.noreply.github.com>
2025-11-09 11:36:22 +0600

    Add admin routes and views for login and dashboard, including layout files

---
+++ resources/views/admin/layouts/auth.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="icon" href="@/assets/images/icons/favicon.svg" type="image/svg+xml"> --}}
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
+
+    <title>{{ config('app.name') }} | @yield('title')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="app">
+        <main class="content-wrapper ">
+            @yield('content')
+        </main>
+    </div>
+    </div>
+</body>
+
+</html>
