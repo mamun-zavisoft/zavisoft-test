51315d71ba17191192eb41964e128f833ac697d2
mannanswe <147591955+mannanswe@users.noreply.github.com>
2025-12-15 16:53:48 +0600

    Add SEO meta tags and improve routing for better search visibility

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,33 @@
 
 <head>
     <meta charset="UTF-8">
+
+    <!-- Responsive -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="icon" href="@/assets/images/icons/favicon.svg" type="image/svg+xml"> --}}
+
+    <!-- Browser compatibility -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Favicon -->
     <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
 
-    <title>{{ config('app.name') }}</title>
+    <!-- Default SEO (Fallback for SPA) -->
+    <title>{{ config('app.name', 'Zavisoft') }}</title>
+
+    <meta name="description" content="Zavisoft - Professional software development company">
+    <meta name="keywords" content="zavisoft, laravel, vue, software company">
+
+    <!-- Open Graph (Social Sharing) -->
+    <meta property="og:title" content="{{ config('app.name', 'Zavisoft') }}">
+    <meta property="og:description" content="Zavisoft - Professional software development company">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:image" content="{{ asset('images/og-image.png') }}">
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+
+    <!-- Assets -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
